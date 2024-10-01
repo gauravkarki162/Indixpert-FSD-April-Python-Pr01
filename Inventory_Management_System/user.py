58250d8feec73a58fc43e673f7756d01fc0900ae
Kumkum555 <Kumkumkapkoti@gmail.com>
2024-10-01 13:48:01 +0530

    uload this code

--- Inventory_Management_System/user.py
+++ Inventory_Management_System/user.py
@@ -1,8 +1,8 @@
-class User:
-    def _init_(self, username, password, first_name, last_name):
-        self.username = username
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-
+class user:
+    def __init__(self,username,password,first_name,last_name):
+        self.username=username
+        self.password=password
+        self.first_name=first_name
+        self.last_name=last_name
+        
 
